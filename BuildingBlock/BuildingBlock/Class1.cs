830046b0e9084106f218c1632d6f21ebfb10a37e
almansorIT <almansor.work@gmail.com>
2026-04-02 15:01:14 +0300

    adding CQRS

--- BuildingBlock/BuildingBlock/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace BuildingBlock;
-
-public class Class1
-{
-
-}
--- Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,20 +1,34 @@
 namespace Catalog.API.Products.CreateProduct;
 
-using MediatR;
+using BuildingBlock.CQRS;
+using Catalog.API.Models;
+
 public record CreateProductCommand(
     string Name,
     List<string> Category,
     string Description,
+    string ImageFile,
     decimal Price
-) : IRequest<CreateProductCommandResponse>;
+) : ICommand<CreateProductCommandResponse>;
 
 public record CreateProductCommandResponse(Guid Id);
 
-public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductCommandResponse>
+public class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductCommandResponse>
 {
-    public Task<CreateProductCommandResponse> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    public async Task<CreateProductCommandResponse> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
-        // Implementation for handling the creation of a product
-        throw new NotImplementedException();
+        //create entity
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            Price = command.Price,
+            ImageFile = command.ImageFile
+        };
+        //save to database
+        
+        //return ProductResult
+        return new CreateProductCommandResponse(Guid.NewGuid());
     }
 }
